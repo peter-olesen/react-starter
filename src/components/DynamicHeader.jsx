0dbb82c71131a07dbeb77017d1bcc56a3d05ceb9
Peter Olesen <63056450+peter-olesen@users.noreply.github.com>
2024-08-13 12:18:02 +0200

    props

--- src/components/DynamicHeader.jsx
+++ src/components/DynamicHeader.jsx
@@ -1,6 +1,12 @@
-export const DynamicHeader =  ({ headerText, className }) => {
+export const DynamicHeader = ({ headerText, className }) => {
 
     return (
         <h1 className={className}>{headerText ? headerText : "Mangler headerText"}</h1>
+    )
+}
+
+export const PropHeader = (props) => {
+    return (
+        <h1>{props.HeaderValue ? props.HeaderValue: "Text Needed"}</h1>
     )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { DynamicHeader } from './DynamicHeader'
+import { DynamicHeader, PropHeader } from './DynamicHeader'
 import { Nav } from './Nav'
 import headerStyles from '../styles/Header.module.scss'
 
@@ -6,6 +6,7 @@ export const Header = () => {
     return (
         <header className={headerStyles.headerStyle}>
             <DynamicHeader className={headerStyles.oppaGangnamStyle} headerText="Props er cool" />
+            <PropHeader HeaderValue="This is the Prop Header" />
             <Nav />
         </header>
     )
